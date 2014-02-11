7a4b9f7ed50e2b2a33a7a9d7dc2430b7a823d224
Eric Dejonckheere <eric@aya.io>
2014-02-11 20:48:17 +0100

    refactor stream methods

--- lib/ayadn/stream.rb
+++ lib/ayadn/stream.rb
@@ -10,66 +10,50 @@ module Ayadn
 			@view.clear_screen
 			print Status.downloading
 			stream = get_data_from_response(@api.get_unified(options))
-			@view.clear_screen
-			if options[:index]
-				@view.show_posts_with_index(stream)
-			else
-				@view.show_posts(stream)
-			end
+			get_view(stream, options)
 		end
 
 		def checkins(options)
 			@view.clear_screen
 			print Status.downloading
 			stream = get_data_from_response(@api.get_checkins(options))
-			@view.clear_screen
-			if options[:index]
-				@view.show_posts_with_index(stream)
-			else
-				@view.show_posts(stream)
-			end
+			get_view(stream, options)
 		end
 
 		def global(options)
 			@view.clear_screen
 			print Status.downloading
 			stream = get_data_from_response(@api.get_global(options))
-			@view.clear_screen
-			if options[:index]
-				@view.show_posts_with_index(stream)
-			else
-				@view.show_posts(stream)
-			end
+			get_view(stream, options)
 		end
 
 		def trending(options)
 			@view.clear_screen
 			print Status.downloading
 			stream = get_data_from_response(@api.get_trending(options))
-			@view.clear_screen
-			if options[:index]
-				@view.show_posts_with_index(stream)
-			else
-				@view.show_posts(stream)
-			end
+			get_view(stream, options)
 		end
 
 		def photos(options)
 			@view.clear_screen
 			print Status.downloading
 			stream = get_data_from_response(@api.get_photos(options))
-			@view.clear_screen
-			if options[:index]
-				@view.show_posts_with_index(stream)
-			else
-				@view.show_posts(stream)
-			end
+			get_view(stream, options)
 		end
 
 		private
 
 		def get_data_from_response(response)
 			response['data']
+		end
+
+		def get_view(stream, options)
+			@view.clear_screen
+			if options[:index]
+				@view.show_posts_with_index(stream)
+			else
+				@view.show_posts(stream)
+			end
 		end
 
 	end
