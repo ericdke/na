5ce044c7bbab0f2eaa92f629a41bbed5f60abe51
Eric Dejonckheere <eric@aya.io>
2014-03-31 00:23:19 +0200

    post and reply tests

--- spec/unit/post_spec.rb
+++ spec/unit/post_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
+require 'helpers'
+require 'json'
+require 'io/console'
 
 describe Ayadn::Post do
   before do
@@ -30,8 +33,36 @@ describe Ayadn::Post do
   end
 
   describe "#check_post_length" do
-    it "checks normal post/message length" do
+    it "checks normal post length" do
       post.check_post_length(["allo", "wtf"]).should be nil #no error
+    end
+  end
+
+  describe "#check_message_length" do
+    it "checks normal message length" do
+      post.check_message_length(["allo", "wtf"]).should be nil #no error
+    end
+  end
+
+  describe "#post" do
+    it "should raise an error if args are empty" do
+      Ayadn::Errors.stub(:warn).and_return("warned")
+      printed = capture_stdout do
+        post.post([])
+      end
+      expect(printed).to include "You should provide some text."
+    end
+  end
+
+  describe "#reply" do
+    it "formats a reply" do
+      new_post = "Hey guys!"
+      replied_to = {1=>{:handle => "@test",:username => "test", :mentions => ["user1", "user2"]}}
+      post.reply(new_post, replied_to).should eq "@test Hey guys! @user1 @user2"
+      replied_to = {1=>{:handle => "@test",:username => "test", :mentions => ["user1", "test"]}}
+      post.reply(new_post, replied_to).should eq "@test Hey guys! @user1"
+      replied_to = {1=>{:handle => "@yo",:username => "test", :mentions => ["test", "lol"]}}
+      post.reply(new_post, replied_to).should eq "@yo Hey guys! @lol"
     end
   end
 
