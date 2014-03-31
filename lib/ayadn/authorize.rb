ff1d4dbdf18d9dbcc3489ed5b902309b7bd3eab9
Eric Dejonckheere <eric@aya.io>
2014-03-31 22:00:16 +0200

    refactored Authorize

--- lib/ayadn/authorize.rb
+++ lib/ayadn/authorize.rb
@@ -1,62 +1,35 @@
 # encoding: utf-8
 module Ayadn
   class Authorize
+
     def authorize
       puts "\e[H\e[2J"
-      if FileOps.old_ayadn?
-        puts "\nAn obsolete version of Ayadn has been detected and will be deleted. Install and authorize the new version? [y/N]\n".color(:red)
-        print "> "
-        answer = STDIN.getch
-        unless answer.downcase == "y"
-          puts Status.canceled
-          exit
-        end
-        puts "\nDeleting old version...\n".color(:green)
-        begin
-          old_dir = Dir.home + "/ayadn"
-          FileUtils.remove_dir(old_dir)
-        rescue => e
-          puts "Unable to remove folder: #{old_dir}\n\n".color(:red)
-          raise e
-        end
-      end
-      home_path = Dir.home + "/ayadn"
-      model = Struct.new(:resp, :username, :id, :handle, :home_path, :user_path)
+      try_remove_old_ayadn
       show_link
-      begin
-        token = STDIN.gets.chomp()
-      rescue Interrupt
-        puts Status.canceled
-        exit
-      rescue => e
-        raise e
-      end
-      if token.empty? || token.nil?
-        puts "\n\nOops, something went wrong, I couldn't get the token. Please try again.\n\n".color(:red)
-        exit
-      end
+      token = get_token
+      check_token(token)
       puts "\n\nThanks! Contacting App.net...\n".color(:green)
-      resp = JSON.parse(RestClient.get("https://alpha-api.app.net/stream/0/users/me?access_token=#{token}", :verify_ssl => OpenSSL::SSL::VERIFY_NONE) {|response, request, result| response })
-      username = resp['data']['username']
-      handle = "@" + username
-      user_path = home_path + "/#{username}"
-      user = model.new(resp, username, resp['data']['id'], handle, home_path, user_path)
-      puts "Ok! Creating Ayadn folders...\n".color(:green)
-      create_config_folders(user)
-      puts "Saving user token...\n".color(:green)
-      create_token_file(user, token)
-      puts "Creating user account for #{user.handle}...\n".color(:green)
-      accounts_db = Daybreak::DB.new("#{home_path}/accounts.db")
-      create_account(user, accounts_db)
+      user = create_user_data(token, Dir.home + "/ayadn")
+      prepare(user)
       puts "Creating configuration...\n".color(:green)
-
-      #now we have a configuration available
       Settings.load_config
       Logs.create_logger
       install
       puts Status.done
       Errors.info "Done!"
       puts "\nThank you for using Ayadn. Enjoy!\n\n".color(:yellow)
+    end
+
+    private
+
+    def prepare(user)
+      puts "Ok! Creating Ayadn folders...\n".color(:green)
+      create_config_folders(user)
+      puts "Saving user token...\n".color(:green)
+      create_token_file(user)
+      puts "Creating user account for #{user.handle}...\n".color(:green)
+      accounts_db = Daybreak::DB.new("#{user.home_path}/accounts.db")
+      create_account(user, accounts_db)
     end
 
     def install
@@ -75,13 +48,8 @@ module Ayadn
       accounts_db.close
     end
 
-    def create_token_file(user, token)
-      unless token.nil? || token.empty?
-        File.write("#{user.user_path}/auth/token", token)
-      else
-        puts Status.wtf
-        exit
-      end
+    def create_token_file(user)
+      File.write("#{user.user_path}/auth/token", user.token)
     end
 
     def create_config_folders(user)
@@ -105,6 +73,57 @@ module Ayadn
       puts "You will then be redirected to a page showing a 'user token' (your secret code).\n".color(:cyan)
       puts "Copy it then paste it here:\n".color(:yellow)
       print "> "
+    end
+
+    def get_user(token)
+      JSON.parse(RestClient.get("https://alpha-api.app.net/stream/0/users/me?access_token=#{token}", :verify_ssl => OpenSSL::SSL::VERIFY_NONE) {|response, request, result| response })
+    end
+
+    def get_token
+      begin
+        token = STDIN.gets.chomp()
+      rescue Interrupt
+        puts Status.canceled
+        exit
+      end
+    end
+
+    def check_token(token)
+      if token.empty? || token.nil?
+        puts "\n\nOops, something went wrong, I couldn't get the token. Please try again.\n\n".color(:red)
+        exit
+      end
+    end
+
+    def try_remove_old_ayadn
+      if FileOps.old_ayadn?
+        answer = ask_del_old_ayadn
+        unless answer.downcase == "y"
+          puts Status.canceled
+          exit
+        end
+        puts "\nDeleting old version...\n".color(:green)
+        begin
+          old_dir = Dir.home + "/ayadn"
+          FileUtils.remove_dir(old_dir)
+        rescue => e
+          puts "Unable to remove folder: #{old_dir}\n\n".color(:red)
+          raise e
+        end
+      end
+    end
+
+    def ask_del_old_ayadn
+      puts "\nAn obsolete version of Ayadn has been detected and will be deleted. Install and authorize the new version? [y/N]\n".color(:red)
+      print "> "
+      STDIN.getch
+    end
+
+    def create_user_data(token, home_path)
+      resp = get_user(token)
+      model = Struct.new(:resp, :username, :id, :handle, :home_path, :user_path, :token)
+      username = resp['data']['username']
+      model.new(resp, username, resp['data']['id'], "@" + username, home_path, home_path + "/#{username}", token)
     end
 
   end
