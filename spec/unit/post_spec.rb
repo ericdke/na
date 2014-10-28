785cf17c3ba9d6ab92ae5706ba7bacccd5a5ed81
Eric Dejonckheere <eric@aya.io>
2014-10-28 23:26:10 +0100

    got rid of annotations specs (included the locale)

--- spec/unit/post_spec.rb
+++ spec/unit/post_spec.rb
@@ -53,7 +53,7 @@ describe Ayadn::Post do
       rest.stub(:post).and_return(File.read("spec/mock/posted.json"))
     end
     it "posts a post" do
-      expect(rest).to receive(:post).with("https://api.app.net/posts/?include_annotations=1&access_token=XYZ", {"text"=>"YOLO", "entities"=>{"parse_markdown_links"=>true, "parse_links"=>true}, "annotations"=>[{"type"=>"com.ayadn.user", "value"=>{"+net.app.core.user"=>{"user_id"=>"@test", "format"=>"basic"}, "env"=>{"platform"=>nil, "ruby"=>"2.1.2", "locale"=>"fr_FR.UTF-8"}}}, {"type"=>"com.ayadn.client", "value"=>{"url"=>"http://ayadn-app.net", "author"=>{"name"=>"Eric Dejonckheere", "username"=>"ericd", "id"=>"69904", "email"=>"eric@aya.io"}, "version"=>"wee"}}]})
+      expect(rest).to receive(:post).with("https://api.app.net/posts/?include_annotations=1&access_token=XYZ", {"text"=>"YOLO", "entities"=>{"parse_markdown_links"=>true, "parse_links"=>true}})
       x = post.post({text: 'YOLO'})
     end
     it "returns the posted post" do
